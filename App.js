449efe3cb67e17be80dc3fff9cc8fe3209923147
MarttinenNoel <noelmarttinen@gmail.com>
2021-02-16 16:42:45 +0200

    tehtävä valmis

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
 
 export default function App() {
+
+  const [text, setText] = React.useState('');
+  const [data, setData] = React.useState([]);
+
+
+
+  const listAdd = () => {
+    setData([... data, {key:text}]);
+    setText("");
+  }
+
+  const listClear = () => {
+    setData([]);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput style={{width:200, borderColor:'gray', borderWidth:1}}
+        onChangeText={text=> setText(text)}
+        value={text}
+      />
+      <Button onPress={listAdd} title="Add" />
+      <Button onPress={listClear} title="clear" />
+
+      <Text>Shopping List</Text>
+      <FlatList
+        data={data}
+        renderItem={({item}) =>
+          <Text>{item.key}</Text> }
+      /> 
       <StatusBar style="auto" />
     </View>
   );
@@ -13,6 +40,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 200,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
